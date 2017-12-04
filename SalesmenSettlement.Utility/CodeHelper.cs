299acfc46e9d72b84abe0a8bcfaf545adc2b5888
qierqiang <lovethesea@qq.com>
2017-12-04 13:33:13 +0800

    添加项目文件。

---
+++ SalesmenSettlement.Utility/CodeHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalesmenSettlement
+{
+    public static class CodeHelper
+    {
+        public static bool IsNullOrBlank(this string source)
+        {
+            return (source == null) || source.Trim().Length == 0;
+        }
+    }
+}

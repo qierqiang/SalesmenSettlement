78c7dc1826620ba13a20ad412b4ef455a1225bbf
qierqiang <lovethesea@qq.com>
2017-12-04 18:56:45 +0800

    first submit

---
+++ SalesmenSettlement.Utility/NotifyPropertyChanged.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace SalesmenSettlement.Utility
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            if (expression.Body is MemberExpression memberExpress && PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(memberExpress.Member.Name));
+            }
+        }
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
